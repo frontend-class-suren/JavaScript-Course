ebb42c830274d191248a68a4155316d2872ac21f
CoderAlive <113170516+Surendrarayamajhi@users.noreply.github.com>
2024-02-01 19:47:18 +0000

    basic data types completed

---
+++ 01_basic_js/02/dataType.js
@@ -0,0 +1,29 @@
+"use strict"; // if using new js version  all JS code than use "use strict"
+
+// alert( 3 + 3) // we are using nodejs, not using browser console so this is not  work
+
+console.log(3 
+    +
+     3) // code readability should be high understandable for all  the dev guyz
+
+console.log("Butwal")
+
+
+let name = "Suren"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => this always should be standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
