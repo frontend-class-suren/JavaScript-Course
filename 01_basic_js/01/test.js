1816534433896b2f5ee7649ead16eb2863bfdd06
unknown <suren.rayamajhi1425@gmail.com>
2024-02-02 14:09:25 +0545

    today 2/2/2024 updated

--- 01_basic_js/01/test.js
+++ 01_basic_js/01/test.js
@@ -1,1 +1,9 @@
-console.log("Suren is Here..")
+// console.log("Suren is Here..")
+// console.log(2**53)
+var str = "400abc"
+var man = "5" //string
+var numberChange = Number(str)
+// console.log(typeof numberChange)
+console.log(numberChange)
+// console.log(typeof str)
+// console.log(str)
--- 01_basic_js/02/1variable.js
+++ 01_basic_js/02/1variable.js
@@ -3,6 +3,7 @@ let accountEmail = "abc@google.com"
 var accountPassword = "123123"
 accountCity = "butwal"
 let accountState;
+// accountId = "53652636"
 
 // accountId = 2 // not allowed
 
@@ -11,7 +12,7 @@ accountEmail = "somarjun@tech.com"
 accountPassword = "21212121"
 accountCity = "sita rice mill"
 
-console.log(accountId);
+// console.log(accountId);
 
 /*
 Prefer not to use var
@@ -20,3 +21,17 @@ because of issue in block scope and functional scope
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// let str1 = "hello"
+// let str2 = "   world"
+// let str3 = str1 + str2
+// console.log(str3)
+
+//Operators
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
