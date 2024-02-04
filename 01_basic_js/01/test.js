0fe99d7dc52e7e5365eb7d77243cf8afa3d7ac8d
unknown <suren.rayamajhi1425@gmail.com>
2024-02-04 12:37:09 +0545

    teszt

--- 01_basic_js/01/test.js
+++ 01_basic_js/01/test.js
@@ -5,5 +5,5 @@ var man = "5" //string
 var numberChange = Number(str)
 // console.log(typeof numberChange)
 console.log(numberChange)
-// console.log(typeof str)
-// console.log(str)
+console.log(typeof str)
+console.log(str)
