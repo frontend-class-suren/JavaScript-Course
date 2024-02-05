fdded10dc076ba45c60f34c6e051cda7eee885bf
sudipak177 <sudiprc177@gmail.com>
2024-02-05 20:14:11 +0545

    practice is completed

--- 01_basic_js/01/sudip.js
+++ 01_basic_js/01/sudip.js
@@ -1,1 +1,45 @@
-console.log(sudip);
+// console.log("sudip");
+
+//homework of feb5//practice
+//
+
+//01//operators 
+//addition//Subtraction//multiplication//Division//modulus
+let a = 5;
+let b = 15;
+let c= 5;
+let d = 10;
+let results = ((((a+b)-c)*c)/5)%2;
+console.log(results);
+// **	Exponentiation (ES2016)
+console.log(5**2);
+
+//comparision operators of javascript
+// == is equal to
+console.log(a==b);
+console.log(a==c);
+let s = "5";
+console.log(a==s);
+
+//=== is strictikly equal to
+console.log(a===s);
+console.log(a===c);
+//!= not equal to
+console.log(a!=d);
+
+//!== not identical
+console.log(a!==s);
+console.log(a!==c);
+
+//> Greater than
+console.log(a>b);
+console.log(a<b);
+
+//>= grater than or equal to
+console.log(a<=b);
+console.log(a>=b);
+
+//JavaScript Bitwise Operators
+
+console.log(a==b & a==s);//like multiplication
+console.log(a==b | a==s);//like addition
