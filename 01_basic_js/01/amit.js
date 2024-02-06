b5fdd73df0a2a22e86448624cd82de0dcd828541
AmitGaire1 <amit.gaire03@gmail.com>
2024-02-06 18:26:20 +0545

    added/deleted

--- 01_basic_js/01/amit.js
+++ 01_basic_js/01/amit.js
@@ -127,6 +127,46 @@ if (operation === "add") {
 
 */
 
+/*
+Function `findGrade()` to find the grade of the student based on the input marks.
+
+Print “S grade” if marks is between 90 and 100.
+Print “A grade” if marks is between 80 and 90.
+Print “B grade” if marks is between 70 and 80.
+Print “C grade” if marks is between 60 and 70.
+Print “D grade” if marks is between 50 and 60.
+Print “E grade” if marks is between 40 and 50.
+Print “Student has failed” if marks is between 0 and 40.
+Else print “Invalid marks”.
+*/
+
+/*
+function findGrade(marks) {
+    if (marks >= 90 && marks <= 100) {
+        console.log("S grade");
+    } else if (marks >= 80 && marks < 90) {
+        console.log("A grade");
+    } else if (marks >= 70 && marks < 80) {
+        console.log("B grade");
+    } else if (marks >= 60 && marks < 70) {
+        console.log("C grade");
+    } else if (marks >= 50 && marks < 60) {
+        console.log("D grade");
+    } else if (marks >= 40 && marks < 50) {
+        console.log("E grade");
+    } else if (marks >= 0 && marks < 40) {
+        console.log("Student has failed");
+    } else {
+        console.log("Invalid marks");
+    }
+}
+
+let studentMarks = 90;
+findGrade(studentMarks);
+
+
+*/
+
 
 
 
