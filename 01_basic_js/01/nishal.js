37aeb8b69c32f5210c8cab8b525b15edfe07d58a
NishalOfficial <nishalpanday029@gmail.com>
2024-02-07 09:27:19 +0545

    done some work

--- 01_basic_js/01/nishal.js
+++ 01_basic_js/01/nishal.js
@@ -44,3 +44,73 @@ if (age >= 18) {
     console.log("You are not eligible to vote yet.");
 }
 // Output: You are eligible to vote.
+
+hw yetr
+// Check if a number is positive, negative, or zero
+function checkNumberType(num) {
+    if (isNaN(num)) {
+        console.log("Variable is not a number");
+    } else {
+        if (num > 0) {
+            console.log("Number is positive");
+        } else if (num < 0) {
+            console.log("Number is negative");
+        } else {
+            console.log("Number is zero");
+        }
+    }
+}
+
+// Find the maximum among four numbers
+function findMaxAmongFour(num1, num2, num3, num4) {
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3) || isNaN(num4)) {
+        console.log("Variable is not a number");
+    } else {
+        let max = num1;
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+        }
+        if (num4 > max) {
+            max = num4;
+        }
+        console.log("The maximum number is:", max);
+    }
+}
+
+// Check if a year is a leap year
+function checkLeapYear(year) {
+    if (isNaN(year)) {
+        console.log("Invalid year");
+    } else {
+        if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+            console.log(year + " is a leap year");
+        } else {
+            console.log(year + " is not a leap year");
+        }
+    }
+}
+
+// Calculate the area of different shapes
+function calculateArea(shape, ...params) {
+    if (isNaN(params.filter(param => isNaN(param)).length)) {
+        switch (shape.toLowerCase()) {
+            case "rectangle":
+                console.log("Area of rectangle:", params[0] * params[1]);
+                break;
+            case "circle":
+                console.log("Area of circle:", Math.PI * Math.pow(params[0], 2));
+                break;
+            case "triangle":
+                const s = (params[0] + params[1] + params[2]) / 2;
+                console.log("Area of triangle:", Math.sqrt(s * (s - params[0]) * (s - params[1]) * (s - params[2])));
+                break;
+            default:
+                console.log("Invalid shape");
+        }
+    } else {
+        console.log("Invalid input");
+    }
+}
