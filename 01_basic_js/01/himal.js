13aa50bfc4feb33d8d8b9b05f7429b9d36b02187
easydoeseat <himalpandey235@gmail.com>
2024-02-08 12:51:39 +0545

    cooking

--- 01_basic_js/01/himal.js
+++ 01_basic_js/01/himal.js
@@ -1,1 +1,83 @@
-var str = "himal branch"
+// “Number is even” if the number is divisible by 2
+
+// let num = 20;
+
+// if (num % 2 === 0) {
+//     console.log("Number is even");
+// } else {
+//     console.log("Number is odd");
+// }
+
+// “Variable is not a number” if isNaN() returns true
+
+// let variable = "123";
+// if (isNaN(variable)) {
+//     console.log("Variable is not a number");
+// } else {
+//     console.log("Variable is a number");
+// }
+
+// let firstName = "Himal";
+// let lastName = "Panday";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+// Find the greatest number
+// if (num1 > num2 && num1 > num3) {
+//     console.log("num1 is the greatest");
+// }
+
+// Determine the type of traingle
+// let side1 = 5
+// let side2 = 3
+// let side3 = 5
+// if (side1 === side2 && side2 === side3) {
+//     console.log("Equilateral triangle.");
+// } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+//     console.log("Isosceles triangle.");
+// } else {
+//     console.log("Scalene traingle");
+// }
+
+
+// Function `findGrade()` to find the grade of the student based on the input marks.
+
+// Print “S grade” if marks is between 90 and 100.
+// Print “A grade” if marks is between 80 and 90.
+// Print “B grade” if marks is between 70 and 80.
+// Print “C grade” if marks is between 60 and 70.
+// Print “D grade” if marks is between 50 and 60.
+// Print “E grade” if marks is between 40 and 50.
+// Print “Student has failed” if marks is between 0 and 40.
+// Else print “Invalid marks”.
+
+
+// function findGrade(marks) {
+//     if (marks >= 90 && marks <= 100) {
+//         console.log("S grade");
+//     } else if (marks >= 80 && marks < 90) {
+//         console.log("A grade");
+//     } else if (marks >= 70 && marks < 80) {
+//         console.log("B grade");
+//     } else if (marks >= 60 && marks < 70) {
+//         console.log("C grade");
+//     } else if (marks >= 50 && marks < 60) {
+//         console.log("D grade");
+//     } else if (marks >= 40 && marks < 50) {
+//         console.log("E grade");
+//     } else if (marks >= 0 && marks < 40) {
+//         console.log("Student has failed");
+//     } else {
+//         console.log("Invalid marks");
+//     }
+// }
+
+// let studentMarks = 90;
+// findGrade(studentMarks);
+
+
+
+// Arithmetic operation
+
+
