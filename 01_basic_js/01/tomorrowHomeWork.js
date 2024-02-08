3f09de1037b9ebfe8ac2e1595addf636cb784dfd
unknown <suren.rayamajhi1425@gmail.com>
2024-02-08 19:49:26 +0545

    home work for tomorrow friday updated

--- 01_basic_js/01/tomorrowHomeWork.js
+++ 01_basic_js/01/tomorrowHomeWork.js
@@ -30,8 +30,91 @@ Print “E grade” if marks is between 40 and 50.
 Print “Student has failed” if marks is between 0 and 40.
 Else print “Invalid marks”.
 */
-console.log("CP-Developer")
-console.log("CP-Developer")
-console.log("CP-Developer")
-console.log("CP-Developer")
-console.log("CP-Developer")
+// console.log("CP-Developer")
+// console.log("CP-Developer")
+// console.log("CP-Developer")
+// console.log("CP-Developer")
+// console.log("CP-Developer")
+
+
+
+
+
+/* home work  for tomorrow 2/9/2024 */
+/*
+
+Exercise list
+Write a program that determines whether a given number is positive or negative.
+Write a program that checks if a number is even or odd.
+Write a program to determine the greater of two numbers.
+Write a program that transforms a numerical grade to a letter grade (e.g grade 10 should display “A”).
+Write a program that calculates the ticket price based on age with the following conditions: age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
+Write a program that determines if a year is a leap year.
+Write a program that calculates a discount based on the purchase amount.Prices equal or over 100 discount have a discount of 20. Prices equal or over 50 have a discount of 10. Otherwise discount is 0
+Write a program that greets the user based on the time of day. Display good morning, good afternnon or good evening based on the time of day when you run the code.
+Write a program that calculates the Body Mass Index (BMI) and categorizes it. The formula for BMI is: weight / (height * height).
+Write a simple number guessing game. Provide a secret number and a guess. Based on those numbers give players clues if their guess is higher, lower or correct.
+
+for loops
+#1 — Print the numbers from 0 to 15
+#2 — Print the numbers from 12 to 24
+#3 — Print the ODD numbers from 7 to 31
+#4 — Print the EVEN numbers from 10 to -20
+#5 — Iterate through all numbers from 1 to 45. Print the following:
+    For multiples of 3 print “Fizz”
+    For multiples of 5 print “Buzz”
+    For multiples of 3 and 5 print “FizzBuzz”
+
+#6 — Print all the elements of the following array
+#7 — Calculate the sum of all the numbers in the following array
+    var numbersArray = [1,13,22,123,49]
+1. for Practice
+Code it at repl.it
+
+Construct for loops that accomplish the following tasks:
+
+Print the numbers 0 - 20, one number per line.
+
+Print only the ODD values from 3 - 29, one number per line.
+
+Print the EVEN numbers 12 down to -14 in descending order, one number per line.
+
+Print the numbers 50 down to 20 in descending order, but only if the numbers are multiples of 3.
+
+Initialize two variables to hold the string 'LaunchCode' and the array [1, 5, 'LC101', 'blue', 42], then construct for loops to accomplish the following tasks:
+
+Print each element of the array to a new line.
+
+Print each character of the string---in reverse order---to a new line.
+
+Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
+
+Define an empty evens array to hold the even numbers and an odds array for the odd numbers.
+
+In the loop, determine if each number is even or odd, then put that number into evens or odds, as appropriate.
+
+Print the arrays to confirm the results. Print evens first. Example: console.log(evens);
+
+Check your solution.
+
+9.9.2. while Practice
+Code it at repl.it
+
+Define three variables for the LaunchCode shuttle---one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
+
+Construct while loops to do the following:
+
+Prompt the user to enter the starting fuel level. The loop should continue until the user enters a positive value greater than 5000 but less than 30000.
+
+Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry by having the loop continue until the user enters an integer from 1 - 7.
+
+Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers. (Hint: The loop should end when there is not enough fuel to boost the crew another 50 km, so the fuel level might not reach 0).
+
+After the loops complete, output the result with the phrase, The shuttle gained an altitude of ___ km.
+
+If the altitude is 2000 km or higher, add "Orbit achieved!"
+
+Otherwise add, "Failed to reach orbit."
+
+Check your solution.
+*/
