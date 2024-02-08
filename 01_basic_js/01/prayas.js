a250744ffa04950c9603ab2b6375a3af72cf288f
unknown <raanaa689@gmail.com>
2024-02-08 10:07:19 +0545

    hw js

--- 01_basic_js/01/prayas.js
+++ 01_basic_js/01/prayas.js
@@ -1,79 +1,88 @@
-let num1 = 2;
-let num2 = 2;
+// let num1 = 2;
+// let num2 = 2;
 
-let additionResult = num1 + num2; // Addition
-// let subtractionResult = num1 - num2; // Subtraction
-// let multiplicationResult = num1 * num2; // Multiplication
-// let divisionResult = num1 / num2; // Division
-// let modulusResult = num1 % num2; // Modulus
-// let exponentiationResult = num1 ** num2; // Exponentiation
+// let additionResult = num1 + num2; // Addition
+// // let subtractionResult = num1 - num2; // Subtraction
+// // let multiplicationResult = num1 * num2; // Multiplication
+// // let divisionResult = num1 / num2; // Division
+// // let modulusResult = num1 % num2; // Modulus
+// // let exponentiationResult = num1 ** num2; // Exponentiation
 
-// let a = 5;
-// let b = "5";
+// // let a = 5;
+// // let b = "5";
 
-// console.log(a == b); // Loose equality, true
-// console.log(a === b); // Strict equality, false
-// console.log(a != b); // Loose inequality, false
-// console.log(a !== b); // Strict inequality, true
-// console.log(a > 3); // Greater than, true
-// console.log(a < 10); // Less than, true
-// console.log(a >= 5); // Greater than or equal to, true
-// console.log(a <= 4); // Less than or equal to, false
+// // console.log(a == b); // Loose equality, true
+// // console.log(a === b); // Strict equality, false
+// // console.log(a != b); // Loose inequality, false
+// // console.log(a !== b); // Strict inequality, true
+// // console.log(a > 3); // Greater than, true
+// // console.log(a < 10); // Less than, true
+// // console.log(a >= 5); // Greater than or equal to, true
+// // console.log(a <= 4); // Less than or equal to, false
 
-// Check if a string is palindrome
-function checkPalindrome(str) {
-    const reversedStr = str.split("").reverse().join("");
-    if (str === reversedStr) {
-        console.log(str + " is a palindrome");
-    } else {
-        console.log(str + " is not a palindrome");
-    }
-}
 
-// Find the factorial of a number
-function calculateFactorial(num) {
-    if (isNaN(num) || num < 0) {
-        console.log("Invalid input");
-    } else {
-        let factorial = 1;
-        for (let i = 1; i <= num; i++) {
-            factorial *= i;
-        }
-        console.log("Factorial of", num + ":", factorial);
-    }
-}
+// let secondNumber = 4;
 
-// Check if a number is prime
-function checkPrime(num) {
-    if (isNaN(num) || num <= 1) {
-        console.log("Invalid input");
-    } else {
-        let isPrime = true;
-        for (let i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
-            console.log(num + " is a prime number");
-        } else {
-            console.log(num + " is not a prime number");
-        }
-    }
-}
+// // Check if secondNumber is even or odd
+// if (typeof secondNumber === 'number') {
+//     if (secondNumber % 2 === 0) {
+//         console.log("Number is even");
+//     } else {
+//         console.log("Number is odd");
+//     }
+// } else {
+//     console.log("Variable is not a number");
+// }
 
-// Convert temperature between Celsius and Fahrenheit
-function convertTemperature(temp, unit) {
-    if (isNaN(temp)) {
-        console.log("Invalid temperature");
-    } else {
-        if (unit.toLowerCase() === "celsius") {
-            console.log(temp + "°C is", (temp * 9 / 5) + 32 + "°F");
-        } else if (unit.toLowerCase() === "fahrenheit") {
-            console.log(temp + "°F is", (temp - 32) * 5 / 9 + "°C");
-        } else {
-            console.log("Invalid unit");
-        }
-    }
-}
+// let numA = 9;
+// let numB = 12;
+
+// // Check if numA is greater than numB
+// if (typeof numA === 'number' && typeof numB === 'number') {
+//     if (numA > numB) {
+//         console.log("numA is the largest");
+//     } else if (numA < numB) {
+//         console.log("numB is the largest");
+//     } else {
+//         console.log("numA and numB are equal");
+//     }
+// }
+
+// let sideLength1 = 3;
+// let sideLength2 = 4;
+// let sideLength3 = 5;
+
+// // Check type of triangle
+// if (sideLength1 === sideLength2 && sideLength2 === sideLength3) {
+//     console.log("Equilateral triangle.");
+// } else if (sideLength1 === sideLength2 || sideLength2 === sideLength3) {
+//     console.log("Isosceles triangle.");
+// } else {
+//     console.log("Scalene triangle.");
+// }
+
+
+// let studentMarks = 85;
+
+// // Check grade based on marks
+// if (typeof studentMarks === 'number') {
+//     if (studentMarks >= 90 && studentMarks <= 100) {
+//         console.log("S grade");
+//     } else if (studentMarks >= 80 && studentMarks < 90) {
+//         console.log("A grade");
+//     } else if (studentMarks >= 70 && studentMarks < 80) {
+//         console.log("B grade");
+//     } else if (studentMarks >= 60 && studentMarks < 70) {
+//         console.log("C grade");
+//     } else if (studentMarks >= 50 && studentMarks < 60) {
+//         console.log("D grade");
+//     } else if (studentMarks >= 40 && studentMarks < 50) {
+//         console.log("E grade");
+//     } else if (studentMarks >= 0 && studentMarks < 40) {
+//         console.log("Student has failed");
+//     } else {
+//         console.log("Invalid marks");
+//     }
+// } else {
+//     console.log("Invalid marks");
+// }
