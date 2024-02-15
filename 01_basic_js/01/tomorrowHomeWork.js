bc1d05c56746c95141d9e4f4bedd3ee04a865b57
unknown <suren.rayamajhi1425@gmail.com>
2024-02-15 14:10:46 +0545

    new commit updated

--- 01_basic_js/01/tomorrowHomeWork.js
+++ 01_basic_js/01/tomorrowHomeWork.js
@@ -149,6 +149,26 @@ Check your solution.
 
 Home work for tomorrow-16-02-2024
 Javascript strings are primitive and immutable: All string methods produces a new string without altering the original string.
+Methods	Description
+charAt()	It provides the char value present at the specified index.
+charCodeAt()	It provides the Unicode value of a character present at the specified index.
+concat()	It provides a combination of two or more strings.
+indexOf()	It provides the position of a char value present in the given string.
+lastIndexOf()	It provides the position of a char value present in the given string by searching a character from the last position.
+search()	It searches a specified regular expression in a given string and returns its position if a match occurs.
+match()	It searches a specified regular expression in a given string and returns that regular expression if a match occurs.
+replace()	It replaces a given string with the specified replacement.
+substr()	It is used to fetch the part of the given string on the basis of the specified starting position and length.
+substring()	It is used to fetch the part of the given string on the basis of the specified index.
+slice()	It is used to fetch the part of the given string. It allows us to assign positive as well negative index.
+toLowerCase()	It converts the given string into lowercase letter.
+toLocaleLowerCase()	It converts the given string into lowercase letter on the basis of host?s current locale.
+toUpperCase()	It converts the given string into uppercase letter.
+toLocaleUpperCase()	It converts the given string into uppercase letter on the basis of host?s current locale.
+toString()	It provides a string representing the particular object.
+valueOf()	It provides the primitive value of string object.
+split()	It splits a string into substring array, then returns that newly created array.
+trim()	It trims the white space from the left and right side of the string.
 
 String length
 String charAt()
