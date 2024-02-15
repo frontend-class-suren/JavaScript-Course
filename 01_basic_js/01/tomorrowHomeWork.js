d92d1345bbf4e10c2c01dd0a6c5514631038b3f3
unknown <suren.rayamajhi1425@gmail.com>
2024-02-15 12:50:31 +0545

    string method homework updated

--- 01_basic_js/01/tomorrowHomeWork.js
+++ 01_basic_js/01/tomorrowHomeWork.js
@@ -143,3 +143,33 @@ Check your solution.
 // Array splice()**
 // Array toSpliced()
 // Array slice()
+
+
+/*
+Javascript strings are primitive and immutable: All string methods produces a new string without altering the original string.
+
+String length
+String charAt()
+String charCodeAt()
+String at()
+String [ ]
+String slice()
+String substring()
+String substr()
+See Also:
+String Search Methods
+String Templates
+String toUpperCase()
+String toLowerCase()
+String concat()
+String trim()
+String trimStart()
+String trimEnd()
+String padStart()
+String padEnd()
+String repeat()
+String replace()
+String replaceAll()
+String split()
+
+*/
