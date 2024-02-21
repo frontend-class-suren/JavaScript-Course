636c134f2fce676d1de05dbea40b4024639c733e
unknown <suren.rayamajhi1425@gmail.com>
2024-02-21 13:41:15 +0545

    homework for tomorrow

---
+++ 01_basic_js/DOM/hom_work/hw-feb_22.js
@@ -0,0 +1,6 @@
+// The easiest way to modify the content of an HTML element is by using the innerHTML property.
+// Finding HTML elements by id
+// Finding HTML elements by tag name
+// Finding HTML elements by class name
+// Finding HTML elements by CSS selectors
+// Finding HTML elements by HTML object collections
