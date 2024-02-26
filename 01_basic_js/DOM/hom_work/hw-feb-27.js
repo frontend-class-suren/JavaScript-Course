547db861c770c43c2c233182ad84251894caa6dd
unknown <suren.rayamajhi1425@gmail.com>
2024-02-26 14:51:32 +0545

    adding how work image

---
+++ 01_basic_js/DOM/hom_work/hw-feb-27.js
@@ -0,0 +1,15 @@
+// The addEventListener() method attaches an event handler to the specified element.
+
+// The addEventListener() method attaches an event handler to an element without overwriting existing event handlers.
+
+// You can add many event handlers to one element.
+
+// You can add many event handlers of the same type to one element, i.e two "click" events.
+
+// You can add event listeners to any DOM object not only HTML elements. i.e the window object.
+
+// The addEventListener() method makes it easier to control how the event reacts to bubbling.
+
+// When using the addEventListener() method, the JavaScript is separated from the HTML markup, for better readability and allows you to add event listeners even when you do not control the HTML markup.
+
+// You can easily remove an event listener by using the removeEventListener() method.
